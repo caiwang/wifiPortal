5a9d2d2db9bd7bf5b9065f7000ec23e48c63467f
michael yin <gt8442b@yahoo.com>
2014-08-20 16:46:06 -0400

    first commit

---
+++ src/main/java/net/wyun/facedetection/web/utils/WYunHeader.java
@@ -0,0 +1,13 @@
+package net.wyun.facedetection.web.utils;
+
+public class WYunHeader {
+	public static final String PROTOCOL_VERSION = "x_ProtocolVersion";
+	public static final String CLIENT_KEY = "x_ClientKey";
+	public static final String CLIENT_ID = "x_DeviceId";
+	public static final String PRODUCT_CODE = "x_ProductCode";
+	public static final String SERVICE_AUTH = "x_ServiceAuth";
+	public static final String METADATA = "metadata";
+	public static final String USER = "user";
+	public static final String PASSWORD = "password";
+	
+}
