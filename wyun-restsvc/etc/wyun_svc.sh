ec38bb0b4c2c100c226f3dff51b4ce978397f94a
michaelyin <gt8442b@yahoo.com>
2014-09-25 17:16:07 -0400

    update the shell script start/stop the service.

--- wyun-restsvc/etc/wyun_svc.sh
+++ wyun-restsvc/etc/wyun_svc.sh
@@ -1,87 +1,63 @@
-#!/bin/sh
-JARFile="WYunRest-0.1.0.war"
-PIDFile="application.pid"
-SPRING_OPTS="-DLOG_FILE=application.log"
+#!/bin/bash
 
-function check_if_pid_file_exists {
-	if [ ! -f $PIDFile ]
-		then
-		    echo "PID file not found: $PIDFile"
-		exit 1
-	fi
+echo "Service [$APP_NAME] - [$1]"
+
+export APP_HOME=/home/wlsp/wlsp
+export APP_NAME=WYunRest-0.1.0
+export APP_PORT=8080
+
+echo "    JAVA_HOME=$JAVA_HOME"
+echo "    APP_HOME=$APP_HOME"
+echo "    APP_NAME=$APP_NAME"
+echo "    APP_PORT=$APP_PORT"
+
+function start {
+    if pkill -0 -f $APP_NAME.war > /dev/null 2>&1
+    then
+        echo "Service [$APP_NAME] is already running. Ignoring startup request."
+        exit 1
+    fi
+    echo "Starting application..."
+    nohup java -jar $APP_HOME/$APP_NAME.war \
+        --spring.config.location=file:$APP_HOME/config/   \
+        < /dev/null > $APP_HOME/logs/app.log 2>&1 &
 }
 
-function check_if_process_is_running {
-	if ps -p $(print_process) > /dev/null
-		then
-		return 0
-	else
-		return 1
-	fi
+function stop {
+    if ! pkill -0 -f $APP_NAME.war > /dev/null 2>&1
+    then
+        echo "Service [$APP_NAME] is not running. Ignoring shutdown request."
+        exit 1
+    fi
+
+    # First, we will try to trigger a controlled shutdown using
+    # spring-boot-actuator
+    curl -X POST http://localhost:$APP_PORT/shutdown < /dev/null > /dev/null 2>&1
+
+    # Wait until the server process has shut down
+    attempts=0
+    while pkill -0 -f $APP_NAME.war > /dev/null 2>&1
+    do
+        attempts=$[$attempts + 1]
+        if [ $attempts -gt 5 ]
+        then
+            # We have waited too long. Kill it.
+            pkill -f $APP_NAME.war > /dev/null 2>&1
+        fi
+        sleep 1s
+    done
 }
 
-function print_process {
-	echo $(<"$PIDFile")
-}
-
-case "$1" in
-		status)
-			check_if_pid_file_exists
-			if check_if_process_is_running
-			then
-			echo $(print_process)" is running"
-			else
-			echo "Process not running: $(print_process)"
-			fi
-		;;
-		stop)
-			check_if_pid_file_exists
-			if ! check_if_process_is_running
-				then
-				echo "Process $(print_process) already stopped"
-				exit 0
-			fi
-			kill -TERM $(print_process)
-			echo -ne "Waiting for process to stop"
-			NOT_KILLED=1
-			for i in {1..20}; do
-			if check_if_process_is_running
-				then
-				echo -ne "."
-				sleep 1
-				else
-				NOT_KILLED=0
-			fi
-			done
-			echo
-			if [ $NOT_KILLED = 1 ]
-			then
-			echo "Cannot kill process $(print_process)"
-			exit 1
-			fi
-			echo "Process stopped"
-		;;
-		start)
-			if [ -f $PIDFile ] && check_if_process_is_running
-			then
-			echo "Process $(print_process) already running"
-			exit 1
-			fi
-			nohup java $SPRING_OPTS -jar $JARFile &
-			echo "Process started"
-		;;
-		restart)
-		$0 stop
-			if [ $? = 1 ]
-			then
-			exit 1
-			fi
-			$0 start
-		;;
-		*)
-			echo "Usage: $0 {start|stop|restart|status}"
-			exit 1
+case $1 in
+start)
+    start
+;;
+stop)
+    stop
+;;
+restart)
+    stop
+    start
+;;
 esac
-
-
 exit 0
