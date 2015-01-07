c9d1b72c639e12594dd8196fdf987e0e367fa76a
michaelyin <gt8442b@yahoo.com>
2015-01-07 01:44:22 -0500

    1. add method to find those non-committed orders 2. add method to find orders in last 24 hours

--- wyun-restsvc/src/main/java/net/wyun/rest/wlsp/repository/ProdOrderRepository.java
+++ wyun-restsvc/src/main/java/net/wyun/rest/wlsp/repository/ProdOrderRepository.java
@@ -8,6 +8,7 @@ import net.wyun.rest.wlsp.client.ProdOrderSvcApi;
 import java.util.Collection;
 import java.util.Date;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,10 +21,12 @@ import org.springframework.stereotype.Repository;
 public interface ProdOrderRepository extends CrudRepository<ProdOrder, Long>{
 	
 	// Find all videos that are shorter than a specified duration
-		public Collection<ProdOrder> findByRectimeLessThan(
+		public Collection<ProdOrder> findByRectimeGreaterThan(
 				// The @Param annotation tells tells Spring Data Rest which HTTP request
 				// parameter it should use to fill in the "duration" variable used to
 				// search for Videos
 				@Param(ProdOrderSvcApi.RECTIME_PARAMETER) Date rectime);
+		
+		public Collection<ProdOrder> findByDelicodeIsNull(final Sort sort);
 
 }
