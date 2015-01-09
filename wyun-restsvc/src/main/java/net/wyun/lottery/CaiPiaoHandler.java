8974039e9e9815fc2d23b5c699a3898730a31ba4
michaelyin <gt8442b@yahoo.com>
2015-01-08 20:54:27 -0500

    It cannot be a service. It is better each time that we create a new handler to process the lottery..

--- wyun-restsvc/src/main/java/net/wyun/lottery/CaiPiaoHandler.java
+++ wyun-restsvc/src/main/java/net/wyun/lottery/CaiPiaoHandler.java
@@ -34,7 +34,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-@Service
 public class CaiPiaoHandler {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CaiPiaoHandler.class);
