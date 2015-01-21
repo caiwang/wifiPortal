9e3fbdbae9423cc2247f645fd4b043694e60bd39
michaelyin <gt8442b@yahoo.com>
2015-01-21 10:20:23 -0500

    unit test.

--- wyun-restsvc/src/test/java/net/wyun/lottery/ScheduledLotteryTasks_Test.java
+++ wyun-restsvc/src/test/java/net/wyun/lottery/ScheduledLotteryTasks_Test.java
@@ -2,11 +2,8 @@ package net.wyun.lottery;
 
 import static org.junit.Assert.*;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Set;
 
-import net.wyun.lottery.ScheduledLotteryTasks;
 import net.wyun.util.DateUtils;
 
 import org.junit.Before;
@@ -24,7 +21,6 @@ public class ScheduledLotteryTasks_Test {
 	@Test
 	public void test_blockedHours() {
 
-		Set<String> blocked = ScheduledLotteryTasks.generateBlockingHours();
 		DateUtils du = new DateUtils();
 		
 		Date d = du.time("2007-01-01 00:05:00"); // 5AM
