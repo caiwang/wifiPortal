55a3f393a0c6ac25d7add3044e3893b3e76a559d
michaelyin <gt8442b@yahoo.com>
2015-02-03 20:23:18 -0500

    add Spring security.

---
+++ wyun-restsvc/src/main/java/net/wyun/wlsp/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package net.wyun.wlsp;
+
+/**
+ * @author Xuecheng
+ *
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private static final String[] UNSECURED_RESOURCE_LIST = new String[] { "/test.html", "/",
+        "/resources/**", "/assets/**", "/css/**", "/webjars/**", "/images/**",
+        "/dandelion-assets/**", "/unauthorized", "/error*", "/users*" };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	
+    	
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/prodorder", "/authsms").permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .defaultSuccessUrl("/d3")
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+        
+       
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    	System.out.println("set up user: " + "wlsp");
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+        
+        auth
+        .inMemoryAuthentication()
+            .withUser("admin").password("admin").roles("USER", "ADMIN");
+         
+    }
+    
+    
+}
