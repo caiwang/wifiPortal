b5bcc7bf6f061d6265ed7e05d649590cbe3334e3
michaelyin <gt8442b@yahoo.com>
2015-02-03 20:22:40 -0500

    disable loggerFiler for now. conflict with spring security.

--- wyun-restsvc/src/main/java/net/wyun/wlsp/DataRestRepoConfig.java
+++ wyun-restsvc/src/main/java/net/wyun/wlsp/DataRestRepoConfig.java
@@ -3,31 +3,26 @@
  */
 package net.wyun.wlsp;
 
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 /**
  * @author Xuecheng
  *
  */
-@Configuration
-@ComponentScan
+//@Configuration //has to disable the loggerfilter because it interferes Spring security
+//@ComponentScan
 public class DataRestRepoConfig extends RepositoryRestMvcConfiguration {
 
-	/*
-	@Override
-	public RequestMappingHandlerMapping repositoryExporterHandlerMapping() {
-		RequestMappingHandlerMapping mapping = super.repositoryExporterHandlerMapping();
-
-		mapping.setInterceptors(new Object[] { new LoggerInterceptor() });
-		return mapping;
-	}
-	
-	*/
 	
 	 @Bean
 	    public FilterRegistrationBean filterRegistrationBean () {
@@ -37,6 +32,7 @@ public class DataRestRepoConfig extends RepositoryRestMvcConfiguration {
 	        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
 	        
 	        registrationBean.setFilter(filter);
+	        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 	        
 	        return registrationBean;
 	    }
